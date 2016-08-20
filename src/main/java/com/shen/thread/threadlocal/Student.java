9498af02cd87476e02185afa0d0dcba647a28079
dimhunter <43543865@qq.com>
2016-08-20 16:10:42 +0800

    fdafsaff

---
+++ src/main/java/com/shen/thread/threadlocal/Student.java
@@ -0,0 +1,15 @@
+package com.shen.thread.threadlocal;
+
+public class Student {
+	
+	private int age = 0;   //年龄
+	 
+    public int getAge() {
+        return this.age;
+    }
+ 
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
