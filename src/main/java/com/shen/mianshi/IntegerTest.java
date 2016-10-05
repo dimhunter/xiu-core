6814546cab87cf87b8bd90e568733b58bd6c7aa1
dimhunter <43543865@qq.com>
2016-10-05 10:08:20 +0800

    fdasfs

---
+++ src/main/java/com/shen/mianshi/IntegerTest.java
@@ -0,0 +1,13 @@
+package com.shen.mianshi;
+
+public class IntegerTest {
+	
+	public static void main(String[] args) {
+		Integer a = 1;
+		Integer b = 1;
+		Integer c = new Integer(1);
+		System.out.println(a==b);
+		System.out.println(a==c);
+	}
+	
+}
